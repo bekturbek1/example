cc2a2d8819dbc67e7a94206a2ee2f4f044a07726
bekturbek1 <nurlan.arstanbekov8@gmail.com>
2023-07-28 10:35:19 +0600

    first commit

---
+++ less9.js
@@ -0,0 +1,70 @@
+// let user = new Object();
+
+// user.name = prompt ("name")
+// user.age = prompt ("age")
+// user.status = prompt ("status")
+// user.hobby = prompt ("hobby")
+// user.job = prompt ("job")
+// user.gendet = prompt ("ddd")
+
+// for (let key in user ){
+//     console.log(`${key}: ${user [key]}`)
+// }
+
+
+
+
+// let schedule = {};
+
+// alert( isEmpty(schedule) ); 
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) ); 
+
+// let pff ={};
+
+// alert ( isEmpty (pff));
+
+// pff[2<4] ="pff"
+
+
+
+// alert ( isEmpty(pff) );
+
+
+// function isEmpty(obj) {
+//     for (let key in obj) {
+     
+//       return false;
+//     }
+//     return true;
+//   }
+
+ 
+
+
+
+// function isEmpty(obj) {
+//     for (let key in obj) {
+//       // если тело цикла начнет выполняться - значит в объекте есть свойства
+//       return false;
+//     }
+//     return true;
+//   }
+
+
+
+
+
+
+
+let task = '';
+while (task !== null ) {
+task = prompt("Введите ваше дела на сегодня");
+
+if (task !== null) {
+console.log( " ваши дела на сегодня" , {task});
+}
+}
+
